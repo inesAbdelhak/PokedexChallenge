ac3af55cab702c2af451958a35c551f0ed61b3a5
inesAbdelhak <113594471+inesAbdelhak@users.noreply.github.com>
2023-06-23 00:23:33 +0200

    start and migration in vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="logo">
+      <img src="./assets/pokemonlogo.png" alt="Logo" />
+    </div>
+
+    <main>
+      <div class="search-container">
+        <input v-model="searchKey" type="text" placeholder=" chercher un pokemon" />
+
+        <button @click="search" class="search-button">
+          <font-awesome-icon icon="search" />
+</button>
+
+<button @click="toggleFilters" class="filter-button">
+    Filtre
+    <font-awesome-icon :icon="['fas', 'chevron-down']" />
+  </button>
+
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app{
+  background-color: #F17068;
+}
+
+.logo{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 25vh; 
+}
+img {
+  max-width: 200px;
+ 
+ 
+}
+
+main {
+  padding: 20px;
+  margin-bottom: 50px;
+
+}
+
+.search-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+input[type="text"] {
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  margin-right: 10px;
+}
+
+.search-button {
+  padding: 10px 20px;
+  background-color: blue;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
-createApp(App).mount('#app')
+
+
+import App from './App.vue'; 
+
+library.add(fas);
+
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
